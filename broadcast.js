cd5d0cd3fe17917cc290d2b936b25e2cc074e0c9
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2023-04-12 20:55:53 +0800

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -3,7 +3,7 @@ switch (global.Fca.Require.FastConfig.BroadCast) {
         try {
             var logger = global.Fca.Require.logger;
                 var Fetch = global.Fca.Require.Fetch;
-                    Fetch.get("https://raw.githubusercontent.com/KENLIEPLAYSPH/PLAYSBOT-GLOBAL/main/FcaCast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
+                    Fetch.get("https://raw.githubusercontent.com/Zachh2/chatbot/main/FCA-ZACH.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
                         global.Fca.Data.BroadCast = JSON.parse(res.body.toString())
                     var random = JSON.parse(res.body.toString())[Math.floor(Math.random() * JSON.parse(res.body.toString()).length)] || "Hey, be careful!";
                 logger.Normal(random);
